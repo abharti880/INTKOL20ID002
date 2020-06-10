241853a5acfb425755402a4f10abdd93348c365d
Subham Mitra <46347405+subham2107@users.noreply.github.com>
2020-06-10 20:20:32 +0530

    Add files via upload

---
+++ DataStructures/SUBHAM_MITRA/QueueGenericImplementation/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueGenericImplementation
+{
+    class MyQueue<T>
+    {
+        int front, rear = -1; T[] queue = new T[100];
+       public void enqueue(T x)
+        {
+            if (rear == 99)
+                Console.WriteLine("QUEUE OVERFLOW");
+            else if (front == -1 || rear == -1)
+            {
+                front = rear = 0;
+                queue[rear] = x;
+            }
+            else
+                queue[++rear] = x;
+        }
+        public T deque()
+        {
+            T x = default(T);
+            if (front == -1 || front > rear)
+                Console.WriteLine("QUEUE UNDERFLOW");
+            else if (front == rear)
+            {
+                front = rear = -1;
+            }
+            else
+            {
+                x = queue[front];
+                ++front;
+            }
+            return x;
+        }
+        public void showstack()
+        {
+
+            Console.WriteLine("Current elements in the queue are:");
+            for (int i = front; i <= rear; i++)
+            {
+                Console.WriteLine("{0} ", queue[i]);
+
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyQueue<char> obj = new MyQueue<char>();
+            obj.deque();
+            obj.enqueue('a');
+            obj.enqueue('b');
+            obj.enqueue('c');
+            
+
+            obj.enqueue('d');
+            obj.enqueue('e');
+
+            obj.showstack();
+            Console.WriteLine("Dequed element is " + obj.deque());
+
+            Console.WriteLine("Dequed element is " + obj.deque());
+            obj.showstack();
+
+
+            obj.enqueue('f');
+            obj.enqueue('g');
+
+            obj.showstack();
+            Console.ReadKey();
+
+
+            MyQueue<double> obj1 = new MyQueue<double>();
+            obj1.deque();
+            obj1.enqueue(1.1);
+            obj1.enqueue(1.2);
+            obj1.enqueue(1.3);
+
+
+            obj1.enqueue(1.4);
+            obj1.enqueue(1.5);
+
+            obj1.showstack();
+            Console.WriteLine("Dequed element is " + obj1.deque());
+
+            Console.WriteLine("Dequed element is " + obj1.deque());
+            obj1.showstack();
+
+
+            obj1.enqueue(1.6);
+            obj1.enqueue(1.7);
+
+            obj1.showstack();
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
---
+++ DataStructures/SUBHAM_MITRA/StackGenericImplementation/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericImplementation
+{
+    class Mystack<T>
+    {
+
+        int top = -1; T[] stack = new T[100];
+        public void push(T x)
+        {
+            if (top == 99)
+                Console.WriteLine("STACK OVERFLOW");
+            else
+                stack[++top] = x;
+        }
+        public T pop()
+        {
+           
+             if (top == -1)
+             {  
+                Console.WriteLine("STACK UNDERFLOW");
+                return default(T); 
+             }
+            else
+            {
+            return stack[top--];
+            }
+        }
+       public void showstack()
+        {
+            if (top == -1)
+                Console.WriteLine("STACK UNDERFLOW");
+            else
+            {
+                Console.WriteLine("Current elements in the stack are:");
+                for (int i = top; i >= 0; i--)
+                {
+                    Console.WriteLine(stack[i]);
+
+                }
+            }
+        }
+    }
+     class Program
+    {
+         static void Main(string[] args)
+        {
+            Mystack<string> obj = new Mystack<string>();
+
+            obj.pop();
+            obj.push("Subham");
+            obj.push("Virat");
+            obj.push("Messi");
+           
+
+            obj.push("Ronaldo");
+            obj.push("Sunil");
+
+            obj.showstack();
+            Console.WriteLine("Popped element is " + obj.pop());
+
+            Console.WriteLine("Popped element is " + obj.pop());
+            obj.showstack();
+
+
+            obj.push("Smriti");
+            obj.push("Mahi");
+
+            obj.showstack();
+            Console.ReadKey();
+
+            Mystack<int> obj1 = new Mystack<int>();
+
+            obj1.pop();
+            obj1.push(10);
+            obj1.push(20);
+            obj1.push(30);
+
+
+            obj1.push(40);
+            obj1.push(50);
+
+            obj1.showstack();
+            Console.WriteLine("Popped element is " + obj1.pop());
+
+            Console.WriteLine("Popped element is " + obj1.pop());
+            obj1.showstack();
+
+
+            obj1.push(60);
+            obj1.push(70);
+
+            obj1.showstack();
+            Console.ReadKey();
+
+        }
+    }
+}
+
