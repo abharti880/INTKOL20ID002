8098b907493a0b5a6a25ac96df1b2f09c54abbea
GemAbhishek <66356468+GemAbhishek@users.noreply.github.com>
2020-06-10 16:52:17 +0530

    Add files via upload

---
+++ DataStructures/Abhishek Kumar (856679)/GenericQueueUsingArray/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericQueueUsingArray
+{
+    class GenericQueue<T>
+    {
+        public int Front { get; set; }
+        public int Rear { get; set; }
+        public int Size { get; set; }
+        public T[] Queue { get; set; }
+        public GenericQueue(int size)
+        {
+            Front = Rear = -1;
+            Size = size;
+            Queue = new T[Size];
+        }
+        public bool Enqueue(T val)
+        {
+            if (Rear == Size - 1 && Front == 0)
+                return false;
+            Rear = (Rear + 1) % Size;
+            Queue[Rear] = val;
+            if (Front == -1)
+                Front++;
+            return true;
+        }
+        public T Dequeue()
+        {
+            if (Front == -1 && Rear == -1)
+            {
+                return default(T);
+            }
+            else if (Rear == Front)
+            {
+                int tem = Front;
+                Front = -1;
+                Rear = -1;
+                return Queue[tem];
+            }
+            else
+            {
+                int tem = Front;
+                Front = (Front + 1) % Size;
+                return Queue[tem];
+            }
+        }
+        public void Print()
+        {
+            Console.WriteLine("Queue:");
+            if (Front == -1 && Rear == -1)
+            {
+                Console.WriteLine("Empty");
+            }
+            else
+            {
+                for (int i = Front; i <= Rear; i++)
+                    Console.WriteLine(Queue[i]);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter size of Queue:");
+            int size = int.Parse(Console.ReadLine());
+            GenericQueue<int?> queue = new GenericQueue<int?>(size);
+            bool exit = false;
+            int ch;
+            while (!exit)
+            {
+                Console.WriteLine("\n******************************");
+                Console.WriteLine("1. Enqueue");
+                Console.WriteLine("2. Dequeue");
+                Console.WriteLine("3. Print");
+                Console.WriteLine("4. Exit");
+                Console.WriteLine("Enter Choice:");
+                Console.WriteLine();
+
+                ch = int.Parse(Console.ReadLine());
+                switch (ch)
+                {
+                    case 1:
+                        Console.WriteLine("Enter value:");
+                        int val = int.Parse(Console.ReadLine());
+                        if (queue.Enqueue(val))
+                            Console.WriteLine(val + " has been inserted into queue");
+                        else
+                            Console.WriteLine("could not insert, Queue is full.");
+                        break;
+                    case 2:
+                        int? top = queue.Dequeue();
+                        if (top == null)
+                            Console.WriteLine("could not Dequeue, Queue is empty");
+                        else
+                            Console.WriteLine(top + " has been deleted from queue");
+                        break;
+                    case 3:
+                        queue.Print();
+                        break;
+                    case 4:
+                        exit = true;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
