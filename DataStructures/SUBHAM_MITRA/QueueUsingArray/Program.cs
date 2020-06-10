15d842f7029d008c6770caaa2abbdc715fec8755
Subham Mitra <46347405+subham2107@users.noreply.github.com>
2020-06-10 17:24:53 +0530

    Add files via upload

---
+++ DataStructures/SUBHAM_MITRA/QueueUsingArray/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueUsingArray
+{
+    class MyQueue
+    {
+        int front,rear = -1; int[] queue = new int[100];
+        void enqueue(int x)
+        {
+            if (rear == 99)
+                Console.WriteLine("QUEUE OVERFLOW");
+            else if (front == -1 || rear == -1)
+            {
+                front = rear = 0;
+                queue[rear] = x;
+            }
+            else
+             queue[++rear] = x; 
+        }
+        int deque()
+        {
+            int x = 0;
+            if ( front== -1||front>rear)
+                Console.WriteLine("QUEUE UNDERFLOW");
+            else if(front==rear)
+            {
+                front=rear = -1;
+            }
+            else
+            {
+                x = queue[front];
+                ++front;
+            }
+            return x;
+        }
+        void showstack()
+        {
+            
+                Console.WriteLine("Current elements in the queue are:");
+                for (int i = front; i <= rear; i++)
+                {
+                    Console.WriteLine("{0} ",queue[i]);
+
+                }
+        }
+        
+        static void Main(string[] args)
+        {
+            MyQueue obj = new MyQueue();
+            obj.deque();
+            obj.enqueue(76);
+            obj.enqueue(85);
+            obj.enqueue(79);
+            // Console.WriteLine(obj.pop());
+
+            obj.enqueue(02);
+            obj.enqueue(13);
+
+            obj.showstack();
+            Console.WriteLine("Dequed element is " + obj.deque());
+
+            Console.WriteLine("Dequed element is " + obj.deque());
+            obj.showstack();
+
+
+            obj.enqueue(102);
+            obj.enqueue(36);
+
+            obj.showstack();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DataStructures/SUBHAM_MITRA/StackUsingArray/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackUsingArray
+{
+    class Mystack
+    {
+        int top = -1;int[] stack=new int[100]; 
+        void push(int x)
+        {
+            if (top == 99)
+                Console.WriteLine("STACK OVERFLOW");
+            else
+                stack[++top] = x;
+        }
+        int pop()
+        {
+            int x=0;
+            if (top==-1)
+                Console.WriteLine("STACK UNDERFLOW");
+            else
+            {
+                x = stack[top];
+                top--;
+            }
+            return x;
+        }
+        void showstack()
+        {
+            if(top==-1)
+                Console.WriteLine("STACK UNDERFLOW");
+            else
+            {
+                Console.WriteLine("Current elements in the stack are:");
+                for (int i=top;i>=0;i--)
+                {
+                    Console.WriteLine(stack[i]);
+                    
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Mystack obj = new Mystack();
+            obj.pop();
+            obj.push(76);
+            obj.push(85);
+            obj.push(79);
+            // Console.WriteLine(obj.pop());
+
+            obj.push(02);
+            obj.push(13);
+
+            obj.showstack();
+            Console.WriteLine("Popped element is " + obj.pop());
+
+            Console.WriteLine("Popped element is " + obj.pop());
+            obj.showstack();
+
+
+            obj.push(102);
+            obj.push(36);
+
+            obj.showstack();
+            Console.ReadKey();
+        }
+    }
+}
