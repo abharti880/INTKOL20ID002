f96168d5dad2cf201ebdb69028dcef2303058962
siddhartha1997 <65113289+siddhartha1997@users.noreply.github.com>
2020-06-10 15:21:55 +0530

    added stack and queue using array

---
+++ Siddhartha_Banerjee(856732)/Queue/Queue/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    class LinearQueue
+    {
+        private int[] ele;
+        private int front;
+        private int rear;
+        private int max;
+        public LinearQueue(int size)
+        {
+            ele = new int[size];
+            front = 0;
+            rear = -1;
+            max = size;
+        }
+        public void insert(int item)
+        {
+            if(rear == max-1)
+            {
+                Console.WriteLine("Queue Overflow");
+                return;
+            }
+            else 
+            {
+                ele[++rear] = item;
+            }
+        }
+        public int delete()
+        {
+            if(front == rear + 1)
+            {
+                Console.WriteLine("Queue is empty");
+                return -1;
+            }
+            else
+            {
+                Console.WriteLine("Deleted element is:" + ele[front]);
+                return ele[front++];
+            }
+        }
+        public void printQueue()
+        {
+            if (front == rear + 1)
+            {
+                Console.WriteLine("Queue is empty");
+            }
+            else
+            {
+                for(int i=front;i<=rear;i++)
+                {
+                    Console.WriteLine("Item[" + (i + 1) + "]:" + ele[i]);
+                }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinearQueue q = new LinearQueue(5);
+            q.insert(10);
+            q.insert(20);
+            q.insert(30);
+            q.insert(40);
+            q.insert(50);
+
+            Console.WriteLine("Items are:");
+            q.printQueue();
+
+            q.delete();
+            q.delete();
+
+            Console.WriteLine("Items are:");
+            q.printQueue();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Siddhartha_Banerjee(856732)/Stack/Stack/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class stack
+    {
+        private int[] ele;
+        private int top;
+        private int max;
+        public stack(int size)
+        {
+            ele = new int[size];
+            top = -1;
+            max = size;
+        }
+        public void push(int item)
+        {
+            if(top==max-1)
+            {
+                Console.WriteLine("Stack Overflow");
+                return;
+            }
+            else 
+            {
+                ele[++top] = item;
+            }
+        }
+        public int pop()
+        {
+            if(top==-1)
+            {
+                Console.WriteLine("Stack Underflow");
+                return -1;
+            }
+            else
+            {
+                Console.WriteLine("Popped Element is:" + ele[top]);
+                return ele[top--];
+            }
+        }
+        public void printStack()
+        {
+            if(top==-1)
+            {
+                Console.WriteLine("Stack Empty");
+                return;
+            }
+            else
+            {
+                for(int i=0;i<=top;i++)
+                {
+                    Console.WriteLine("Item[" + (i + 1) + "]:" + ele[i]);
+                }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            stack s = new stack(5);
+            s.push(10);
+            s.push(20);
+            s.push(30);
+            s.push(40);
+            s.push(50);
+
+            Console.WriteLine("Items are:");
+            s.printStack();
+
+            s.pop();
+            s.pop();
+            s.pop();
+            Console.ReadKey();
+        }
+    }
+}
