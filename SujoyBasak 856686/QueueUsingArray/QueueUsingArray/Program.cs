1cf9b9cb25ce1d1ff41eae580f46fabe3e93ebfc
Sujoy Basak <66714714+SujoyBasak@users.noreply.github.com>
2020-06-10 15:10:59 +0530

    Add files via upload

---
+++ SujoyBasak 856686/QueueUsingArray/QueueUsingArray/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace QueueUsingArray
+{
+    class Queue
+    {
+        int size = 10;
+        int[] array = new int[100];        
+        int front = -1, rear = -1;
+
+
+        public bool push(int data)
+        {
+            if (rear == size - 1)
+            {
+                Console.WriteLine("Overflow");
+                return false;
+            }
+            else if(front==-1 && rear==-1)
+            {
+                front = 0;
+                rear = 0;
+                array[rear] = data;
+            }
+            else
+                array[++rear] = data;
+            Console.WriteLine("Data Inserted");
+            return true;
+        }
+
+        public bool pop()
+        {
+            if (front == -1 || front > rear)
+            {
+                Console.WriteLine("\nUnderflow\n");
+                return false;
+
+            }
+            else if (front ==rear)
+            {
+                front = -1;
+                rear = -1;
+            }
+            else
+            {
+                ++front;
+                
+            }
+                
+
+            return true;
+        }
+        public bool show()
+        {
+            if (rear == -1)
+            {
+                Console.WriteLine("Empty Queue");
+                return false;
+            }
+
+                
+
+            for(int i=front;i<=rear;i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            return true;
+            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int flag = 0;
+            Queue q = new Queue();
+            do
+            {
+                Console.WriteLine("Enter 1 to Show");
+                Console.WriteLine("Enter 2 to Push");
+                Console.WriteLine("Enter 3 to Pop\n");
+                Console.Write("Enter Choice");
+                int key = int.Parse(Console.ReadLine());
+
+                if (key == 1)
+                    q.show();
+                else if (key == 2)
+                {
+                    Console.Write("Enter Data: ");
+                    int data = int.Parse(Console.ReadLine());
+                    q.push(data);
+                }
+                else if (key == 3)
+                    q.pop();
+
+            }
+            while (flag != 1);
+            
+
+
+        }
+    }
+}
---
+++ SujoyBasak 856686/StackUsingArray/StackUsingArray/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace StackUsingArray
+{
+    
+
+
+    class Stack
+    {
+        int[] array = new int[10];
+        int size = 10;
+        int top = -1;
+
+        public bool push(int data)
+        {
+            if (top == size - 1)
+            {
+                Console.WriteLine("\nOverflow\n");
+                return false;
+            }
+
+            array[++top] = data;
+            Console.WriteLine("\nData Stacked\n");
+
+            return true;
+        }
+
+        public bool pop()
+        {
+            if (top == -1)
+            {
+                Console.WriteLine("\nStack is Empty\n");
+                return false;
+            }
+            --top;
+            Console.WriteLine("\nData Poped\n");
+            return true;
+        }
+
+        public void show()
+        {
+            if (top == -1)
+                Console.WriteLine("\nStack is Empty\n");
+
+            else
+            {
+                Console.WriteLine("\n Stack: ");
+
+                for (int i = top; i >= 0; i--)
+                    Console.WriteLine(" " + array[i]);
+
+                Console.WriteLine("\n");
+            }
+
+        }
+
+        public void peek()
+        {
+            if (top == -1)
+                Console.WriteLine("\nStack is Empty\n");
+
+            Console.WriteLine("\n" + array[top] + "\n");
+        }
+
+    }
+
+    class Program   
+    {
+        static void Main(string[] args)
+        {
+            int flag = 0;
+            Stack st = new Stack();
+            do
+            {
+                Console.WriteLine("Enter 1 To Show");
+                Console.WriteLine("Enter 2 to Push");
+                Console.WriteLine("Enter 3 to Pop");
+                Console.WriteLine("Enter 4 to Peek");
+                Console.WriteLine("Enter 5 to Exit\n");
+
+                Console.Write("Enter Choice: ");
+                int key = int.Parse(Console.ReadLine());
+
+
+                if (key == 1)
+                    st.show();
+
+                else if (key == 2)
+                {
+                    Console.Write("\nEnter Data: ");
+                    int data = int.Parse(Console.ReadLine());
+                    st.push(data);
+                }
+                else if (key == 3)
+                    st.pop();
+                else if (key == 4)
+                    st.peek();
+                else
+                {
+                    Console.WriteLine("\nClosing...");
+                    flag = 1;
+                }
+
+            }
+            while (flag != 1);
+
+
+
+
+        }
+    }
+}
