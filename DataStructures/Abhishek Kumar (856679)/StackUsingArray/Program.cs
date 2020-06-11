6f836a885220e4967c429319b841dce12f515b78
GemAbhishek <66356468+GemAbhishek@users.noreply.github.com>
2020-06-11 11:27:25 +0530

    Update Program.cs
    added clear() and Count()

--- DataStructures/Abhishek Kumar (856679)/StackUsingArray/Program.cs
+++ DataStructures/Abhishek Kumar (856679)/StackUsingArray/Program.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -41,6 +42,18 @@ namespace StackUsingArray
                 Console.WriteLine("Top element is {0}", Stack[Top]);
             }
         }
+        public int Count()
+        {
+            return Top;
+        }
+
+        
+        public void Clear()
+        {
+            Top = -1;
+            Console.WriteLine("Stack cleared");
+        }
+
         public void Print()
         {
             Console.WriteLine("Stack:");
@@ -71,7 +84,9 @@ namespace StackUsingArray
                 Console.WriteLine("2. Pop");
                 Console.WriteLine("3. Print");
                 Console.WriteLine("4. Peek");
-                Console.WriteLine("5. Exit");
+                Console.WriteLine("5. Count");
+                Console.WriteLine("6. Clear ");
+                Console.WriteLine("7. Exit");
                 Console.WriteLine("Enter Choice:");
                 ch = int.Parse(Console.ReadLine());
                 switch (ch)
@@ -98,6 +113,20 @@ namespace StackUsingArray
                         stack.peek();
                         break;
                     case 5:
+                        if(stack.Top == -1)
+                        {
+                            Console.WriteLine("Empty");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Count = {0}",stack.Top+1);
+                        }
+
+                        break;
+                    case 6:
+                        stack.Clear();
+                        break;
+                    case 7:
                         exit = true;
                         break;
                     default:
