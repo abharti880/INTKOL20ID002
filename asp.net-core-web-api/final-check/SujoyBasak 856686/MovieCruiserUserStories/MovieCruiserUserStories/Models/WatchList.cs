5415d71836da07e966b64b6d43e99625df8d5c6f
Sujoy Basak <66714714+SujoyBasak@users.noreply.github.com>
2020-06-16 18:20:42 +0530

    Add files via upload

---
+++ asp.net-core-web-api/final-check/SujoyBasak 856686/MovieCruiserUserStories/MovieCruiserUserStories/Models/WatchList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieCruiserUserStories.Models
+{
+    public class WatchList
+    {
+        [Key]
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+        public DateTime DateOfLaunch { get; set; }
+        public string Genre { get; set; }
+        public bool HasTeaser { get; set; }
+        public bool Favorite { get; set; }
+
+    }
+}
