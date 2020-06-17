950080e9c7626d310d6848be7d10e2206a51380b
Subham Mitra <46347405+subham2107@users.noreply.github.com>
2020-06-17 17:39:17 +0530

    Add files via upload

---
+++ asp.net-core-web-api/final-check/Subham Mitra_856729/Final Check (Web_Api)/MovieCruiserUserStories/Models/Favorites.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieCruiserUserStories.Models
+{
+    public class Favorites
+    {
+        [Key]
+        public int Id { get; set; }
+        public string[] Movies { get; set; }
+        public int Count { get; set; }
+
+    }
+}
