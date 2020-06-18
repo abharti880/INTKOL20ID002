627a772f9d4aaa33b54754f8a29b987742b578a7
Dishanidas005 <66713712+Dishanidas005@users.noreply.github.com>
2020-06-18 14:03:53 +0530

    Add files via upload

---
+++ asp.net-core-web-api/practice-check/PRACTICE CHECK DISHANIDAS/TruYum/TruYum/Controllers/MenuItemController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TruYum.Models;
+
+namespace TruYum.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuItemController : ControllerBase
+    {
+        // GET: api/MenuItem
+        [HttpGet]
+        public IEnumerable<MenuItem> Get()
+        {
+            MenuItemOperation mo = new MenuItemOperation();
+            return mo.SelectMenuItems();
+        }
+
+        // GET: api/MenuItem/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/MenuItem
+        [HttpPost]
+        public string Post([FromBody] MenuItem item)
+        {
+            MenuItemOperation mo = new MenuItemOperation();
+            mo.InsertMenuItem(item);
+            return "Successfully Inserted";
+        }
+
+        // PUT: api/MenuItem/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ asp.net-core-web-api/practice-check/PRACTICE CHECK DISHANIDAS/TruYum/TruYum/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TruYum.Models;
+
+namespace TruYum.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        // GET: api/User
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/User/5
+        [HttpGet("{id}")]
+        public bool Get(int id,[FromBody] string password)
+        {
+            UserOperation uo = new UserOperation();
+            return uo.Login(id,password);
+        }
+
+        // POST: api/User
+        [HttpPost]
+        public string Post([FromBody] User user)
+        {
+            UserOperation uo = new UserOperation();
+            return uo.CreateUser(user);
+        }
+
+        // PUT: api/User/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
