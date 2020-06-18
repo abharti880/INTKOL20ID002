ffabefbc4044e5c72f15d4c248a4166f166281ff
arghyadeep6 <66715068+arghyadeep6@users.noreply.github.com>
2020-06-18 18:54:32 +0530

    Add files via upload

---
+++ asp.net-core-web-api/final-check/ARGHYADEEP SAHA(856716) FINAL CHECK/MovieCruiserUserStories/Controllers/AuthController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace MovieCruiserUserStories.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return GenerateJSONWebToken(id);
+        }
+
+
+
+        private string GenerateJSONWebToken(int userId)
+        {
+            string userRole = "";
+
+            if (userId == 1)
+                userRole = "Admin";
+
+            else if (userId != -1 && userId != 1)
+                userRole = "Customer";
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("mysuperdupersecret"));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Role, userRole),
+                new Claim("UserId", userId.ToString())
+            };
+            var token = new JwtSecurityToken(
+            issuer: "mySystem",
+            audience: "myUsers",
+            claims: claims,
+            expires: DateTime.Now.AddMinutes(10),
+            signingCredentials: credentials);
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
