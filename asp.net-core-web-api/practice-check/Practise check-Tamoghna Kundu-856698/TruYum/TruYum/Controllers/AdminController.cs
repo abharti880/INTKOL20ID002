6a9692134e7b6cbb68420e777e2acb112e615d3c
Tamoghna865 <66713797+Tamoghna865@users.noreply.github.com>
2020-06-18 14:13:41 +0530

    Add files via upload

---
+++ asp.net-core-web-api/practice-check/Practise check-Tamoghna Kundu-856698/TruYum/TruYum/Controllers/AdminController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TruYum.Models;
+
+namespace TruYum.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "Admin")]
+    public class AdminController : ControllerBase
+    {
+        // GET: api/Admin
+        [HttpGet]
+        public IEnumerable<MenuItem> Get()
+        {
+            MenuItemOperation mo = new MenuItemOperation();
+            return mo.SelectMenuItems();
+        }
+
+        // GET: api/Admin/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Admin
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Admin/5
+        [HttpPut("{id}")]
+        public string Put(int id, [FromBody] MenuItem item)
+        {
+            MenuItemOperation mo = new MenuItemOperation();
+            mo.UpdateMenuItem(id,item);
+            return "Successfully Updated";
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
