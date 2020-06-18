912e1c3db98a57ba9bcbab1124fd94b9cf556b74
biswa9898 <66714100+biswa9898@users.noreply.github.com>
2020-06-18 14:01:34 +0530

    Add files via upload

---
+++ asp.net-core-web-api/final-check/BISWARUP CHAKRABORTY(856719)/MovieCruiserUserStories/Models/Member.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieCruiserUserStories.Models
+{
+    public class Member
+    {
+        [Key]
+        public string userName { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string password { get; set; }
+        public string confirmPassword { get; set; }
+
+    }
+}
