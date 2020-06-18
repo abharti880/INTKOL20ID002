bb1674b31e00cbb6321b98e544ee4c419b9a3136
arghyadeep6 <66715068+arghyadeep6@users.noreply.github.com>
2020-06-18 18:50:52 +0530

    Add files via upload

---
+++ asp.net-core-web-api/practice-check/ARGHYADEEP SAHA(856716) PRACTICE/TruYum/DAO/DbCon.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TruYum.Models;
+
+namespace TruYum.DAO
+{
+    public class DbCon:DbContext
+    {
+        public DbCon(DbContextOptions<DbCon> options):base(options)
+        {
+
+        }
+
+        public DbSet<MenuItem> MenuItem { get; set; }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Cart> Cart { get; set; }
+        public DbSet<User> User { get; set; }
+    }
+}
