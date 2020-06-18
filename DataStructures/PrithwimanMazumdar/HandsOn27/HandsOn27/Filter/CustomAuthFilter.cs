f14cd925caf00b6da5b2fc369bf910775ea1a475
Prithwiman Mazumdar <30746565+prithwiman97@users.noreply.github.com>
2020-06-18 20:06:45 +0530

    Add files via upload

---
+++ DataStructures/PrithwimanMazumdar/HandsOn27/HandsOn27/Filter/CustomAuthFilter.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Stripe.Issuing;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Rewrite.Internal.UrlActions;
+
+namespace HandsOn27.Filter
+{
+    public class CustomAuthFilter:ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            string authheader = filterContext.HttpContext.Request.Headers["Authorization"];
+            if(authheader==null)
+            {
+                filterContext.Result = new BadRequestObjectResult("Invalid request - No Auth token");
+            }
+            else if(!authheader.StartsWith("Bearer"))
+            {
+                filterContext.Result = new BadRequestObjectResult("Invalid request - Token present but Bearer Unavailable");
+            }
+        }
+    }
+}
---
+++ DataStructures/PrithwimanMazumdar/JWT_31/JWT_31/Controllers/AuthController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using JWT_31.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Razor.Language.Extensions;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JWT_31.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [AllowAnonymous]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post([FromBody] User u)
+        {
+            /*var authorizationHeader = Request.Headers["Authorization"].First();
+            var key = authorizationHeader.Split(' ')[1];
+            var credentials = Encoding.UTF8.GetString(Convert.FromBase64String(key)).Split(':');*/
+            //var serverSecret = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWTDemo:ServerSecret"]));
+            if (u.Username == "hello" && u.Password == "123")
+            {
+                string role = "";
+                if (u.ID == 1)
+                    role = "Admin";
+                else
+                    role = "Customer";
+                var result = new
+                {
+                    token = GenerateJSONWebToken(u.ID,role)
+                };
+
+                return Ok(result);
+            }
+            return BadRequest();
+        }
+        private string GenerateJSONWebToken(int userId, string userRole)
+
+        {
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("mysuperdupersecret"));
+
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new List<Claim>
+            {
+
+                new Claim(ClaimTypes.Role, userRole),
+
+                new Claim("UserId", userId.ToString())
+
+            };
+
+            var token = new JwtSecurityToken(
+
+            issuer: "mySystem",
+
+            audience: "myUsers",
+
+            claims: claims,
+
+            expires: DateTime.Now.AddMinutes(10),
+
+            signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+
+        }
+    }
+}
