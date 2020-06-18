3f112ce8caf5c3e003c7dde06e0b72d208978bc1
Asmita98 <66713908+Asmita98@users.noreply.github.com>
2020-06-18 14:06:32 +0530

    Add files via upload

---
+++ asp.net-core-web-api/final-check/Asmita Chakraborty 856673 Final/MovieCruiserUserStories/Controllers/AnonymousController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieCruiserUserStories.DAO;
+using MovieCruiserUserStories.Models;
+
+namespace MovieCruiserUserStories.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnonymousController : ControllerBase
+    {
+        private readonly DbCon con;
+        public AnonymousController(DbCon _con)
+        {
+            con = _con;
+        }
+        [HttpGet]
+        public IEnumerable<Movies> Get()
+        {
+            return con.Movies.ToList();
+        }
+    }
+}
