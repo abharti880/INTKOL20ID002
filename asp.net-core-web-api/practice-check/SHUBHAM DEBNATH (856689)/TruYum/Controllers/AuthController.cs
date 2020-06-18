67af9a2dd2f26d8a588c7a672eda096a190ba275
shubhamagain7 <65859337+shubhamagain7@users.noreply.github.com>
2020-06-18 14:07:23 +0530

    Add files via upload

---
+++ asp.net-core-web-api/practice-check/SHUBHAM DEBNATH (856689)/TruYum/Controllers/AuthController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using TruYum.Models;
+
+namespace TruYum.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post([FromBody] User u)
+        {
+            UserOperation operation = new UserOperation();
+            if (operation.Login(u.Id,u.Password))
+            {
+                string role = "";
+                if (u.Id == 1)
+                    role = "Admin";
+                else if(u.Id > 1)
+                    role = "Customer";
+                var result = new
+                {
+                    token = GenerateJSONWebToken(u.Id, role)
+                };
+                return Ok(result);
+            }
+            return BadRequest();
+        }
+        private string GenerateJSONWebToken(int userId, string userRole)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("mysuperdupersecret"));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Role, userRole),
+                new Claim("UserId", userId.ToString())
+            };
+            var token = new JwtSecurityToken(
+                issuer: "mySystem",
+                audience: "myUsers",
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(10),
+                signingCredentials: credentials
+            );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
