584c371ee27470939a254b5104bd1ad0193045b6
SS524 <soumyadeepsaha7@gmail.com>
2020-06-18 17:31:05 +0530

    Add files via upload

---
+++ asp.net-core-web-api/practice-check/Soumyadeep Saha(856724)Practice_Check/TruYum/Controllers/AnonymousUserController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TruYum.DAO;
+using TruYum.Models;
+
+namespace TruYum.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [AllowAnonymous]
+    public class AnonymousUserController : ControllerBase
+    {
+        private readonly DbCon con;       
+
+        public AnonymousUserController(DbCon _con)
+        {
+            con = _con;
+        }
+        // GET: api/AnonymousUser
+        [HttpGet]
+        public IEnumerable<MenuItem> Get()
+        {
+            Category cat;
+            List<MenuItem> list = new List<MenuItem>();
+            
+            var menu= con.MenuItem.ToList();
+            
+            foreach(var x in menu)
+            {
+                if(x.categoryId==1)
+                {
+                    cat = new Category();
+                    cat.Id = x.categoryId;
+                    cat.Name = "Starter";
+                    x.Category = cat;
+                }
+                else if(x.categoryId==2)
+                {
+                    cat = new Category();
+                    cat.Id = x.categoryId;
+                    cat.Name = "Main Course";
+                    x.Category = cat;
+                }
+                else if(x.categoryId==3)
+                {
+                    cat = new Category();
+                    cat.Id = x.categoryId;
+                    cat.Name = "Drinks";
+                    x.Category = cat;
+                }
+                else if(x.categoryId==4)
+                {
+                    cat = new Category();
+                    cat.Id = x.categoryId;
+                    cat.Name = "Desert";
+                    x.Category = cat;
+                }
+                list.Add(x);
+            }
+
+            return list;
+        }
+
+        
+    }
+}
---
+++ asp.net-core-web-api/practice-check/Soumyadeep Saha(856724)Practice_Check/TruYum/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TruYum.DAO;
+using TruYum.Models;
+
+namespace TruYum.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly DbCon con;
+        public UserController(DbCon _con)
+        {
+            con = _con;
+        }
+        [HttpGet("{id}")]
+        public bool Get(int id,[FromBody]string password)
+        {
+            var data = con.User.ToList();
+            foreach(var cred in data)
+            {
+                if (cred.userId == id && cred.password == password)
+                    return true;                
+            }
+            return false;
+        }
+        [HttpPost]
+        public string Post([FromBody]User cred)
+        {
+            con.User.Add(cred);
+            con.SaveChanges();
+
+            return "Successfully Registered";
+        }
+    }
+}
