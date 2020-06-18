29bd0da3803097d352a160e0a765321d336d6a79
SS524 <soumyadeepsaha7@gmail.com>
2020-06-18 17:21:03 +0530

    Add files via upload

---
+++ asp.net-core-web-api/final-check/Soumyadeep Saha(856724)Final_Check/MovieCruiserUserStories/Controllers/AdminController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieCruiserUserStories.DAO;
+using MovieCruiserUserStories.Models;
+
+namespace MovieCruiserUserStories.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "Admin")]
+    public class AdminController : ControllerBase
+    {
+        private readonly DbCon con;
+        public AdminController(DbCon _con)
+        {
+            con = _con;
+        }
+
+
+        [HttpGet]
+        public IEnumerable<Movies> Get()
+        {
+            return con.Movies.ToList();
+        }
+
+
+        [HttpPut("{id}")]
+        public string Put(int id,[FromBody]Movies movie)
+        {
+            var data = con.Movies.Find(id);
+            if (data != null)
+            {
+                data.Id = movie.Id;
+                data.Title = movie.Title;
+                data.BoxOffice = movie.BoxOffice;
+                data.Active = movie.Active;
+                data.DateOfLaunch = movie.DateOfLaunch;
+                data.Genre = movie.Genre;
+                data.HasTeaser = movie.HasTeaser;
+                //data.Favorite = movie.Favorite;
+
+                con.SaveChanges();
+            }
+            else
+                return "Id is not in the Database";
+
+            return "Movie Updated";
+            
+        }
+    }
+}
---
+++ asp.net-core-web-api/final-check/Soumyadeep Saha(856724)Final_Check/MovieCruiserUserStories/Controllers/MoviesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieCruiserUserStories.DAO;
+using MovieCruiserUserStories.Models;
+
+namespace MovieCruiserUserStories.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly DbCon con;
+
+        public MoviesController(DbCon _con)
+        {
+            con = _con;
+        }
+
+
+
+        [HttpGet]
+        public IEnumerable<Movies> Get()
+        {
+            return con.Movies.ToList();
+        }
+        [HttpPost]
+        public string Post([FromBody] Movies movie)
+        {
+            con.Movies.Add(movie);
+            con.SaveChanges();
+            return "Movie Added to Database";
+        }
+    }
+}
