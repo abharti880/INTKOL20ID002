f80cb306904b30ae38f5126a646f9f1c600e7d14
Asmita98 <66713908+Asmita98@users.noreply.github.com>
2020-06-18 14:10:23 +0530

    Add files via upload

---
+++ asp.net-core-web-api/practice-check/Asmita Chakraborty 856673 Practice/TruYum/TruYum/Models/CartOperation.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TruYum.Models
+{
+    public class CartOperation
+    {
+        SqlConnection con;
+        public CartOperation()
+        {
+            con= new SqlConnection(@"Data Source=DESKTOP-D0P8NMI\SQLEXPRESS;Initial Catalog=TruYum;Integrated Security=true");
+        }
+        public string AddToCart(Cart cart)
+        {
+            con.Open();
+            string cmd = "INSERT INTO cart VALUES("+cart.Id+","+cart.ItemId+")";
+            SqlCommand insert = new SqlCommand(cmd,con);
+            insert.ExecuteNonQuery();
+            con.Close();
+            return "Cart Created";
+        }
+        public List<MenuItem> GetCartItems(User user)
+        {
+            con.Open();
+            string cmd = "SELECT m.Id,m.Name,m.Price,m.FreeDelivery,m.DateOfLaunch,m.Active FROM users u,cart c,menuitem m WHERE u.Id="+user.Id+" AND u.CartId=c.Id AND c.ItemId=m.Id";
+            SqlCommand select = new SqlCommand(cmd, con);
+            SqlDataAdapter da = new SqlDataAdapter(select);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            List<MenuItem> items = new List<MenuItem>();
+            for(int i=0;i<dt.Rows.Count;i++)
+            {
+                MenuItem mi = new MenuItem();
+                mi.Id = Convert.ToInt32(dt.Rows[i]["Id"]);
+                mi.Name = dt.Rows[i]["Name"].ToString();
+                mi.FreeDelivery = Convert.ToBoolean(dt.Rows[i]["FreeDelivery"]);
+                mi.Price = Convert.ToDouble(dt.Rows[i]["Price"]);
+                mi.DateofLaunch = Convert.ToDateTime(dt.Rows[i]["DateOfLaunch"]);
+                mi.Active = Convert.ToBoolean(dt.Rows[i]["Active"]);
+                items.Add(mi);
+            }
+            return items;
+        }
+        public string DeleteFromCart(int ItemId)
+        {
+            con.Open();
+            string cmd = "DELETE FROM Cart WHERE Id=1 AND ItemId="+ItemId+";";
+            SqlCommand delete = new SqlCommand(cmd, con);
+            delete.ExecuteNonQuery();
+            con.Close();
+            return "Deleted from Cart = "+ItemId;
+        }
+    }
+}
---
+++ asp.net-core-web-api/practice-check/Asmita Chakraborty 856673 Practice/TruYum/TruYum/Models/MenuItemOperation.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Query.ExpressionVisitors.Internal;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TruYum.Models
+{
+    public class MenuItemOperation
+    {
+        SqlConnection con;
+        public MenuItemOperation()
+        {
+            con = new SqlConnection(@"Data Source=DESKTOP-D0P8NMI\SQLEXPRESS;Initial Catalog=TruYum;Integrated Security=true");
+        }
+        public void InsertMenuItem(MenuItem item)
+        {
+            con.Open();
+            int Id = item.Id;
+            string Name = item.Name;
+            int fd = Convert.ToInt32(item.FreeDelivery);
+            double Price = item.Price;
+            DateTime date = item.DateofLaunch;
+            int active = Convert.ToInt32(item.Active);
+            string cmd = "INSERT INTO menuitem VALUES("+Id+",'"+Name+"',"+fd+","+Price+",'"+date+"',"+active+")";
+            SqlCommand insert = new SqlCommand(cmd,con);
+            insert.ExecuteNonQuery();
+            con.Close();
+        }
+        public List<MenuItem> SelectMenuItems()
+        {
+            con.Open();
+            SqlCommand select = new SqlCommand("SELECT * FROM menuitem", con);
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(select);
+            da.Fill(dt);
+            if (dt.Rows.Count>0)
+            {
+                List<MenuItem> items = new List<MenuItem>();
+                for(int i=0;i<dt.Rows.Count;i++)
+                {
+                    MenuItem mi = new MenuItem();
+                    mi.Id = Convert.ToInt32(dt.Rows[i]["Id"]);
+                    mi.Name = dt.Rows[i]["Name"].ToString();
+                    mi.FreeDelivery = Convert.ToBoolean(dt.Rows[i]["FreeDelivery"]);
+                    mi.Price = Convert.ToDouble(dt.Rows[i]["Price"]);
+                    mi.DateofLaunch = Convert.ToDateTime(dt.Rows[i]["DateOfLaunch"]);
+                    mi.Active = Convert.ToBoolean(dt.Rows[i]["Active"]);
+                    items.Add(mi);
+                }
+                return items;
+            }
+            return null;
+        }
+        public void UpdateMenuItem(int id,MenuItem item)
+        {
+            con.Open();
+            string Name = item.Name;
+            int fd = Convert.ToInt32(item.FreeDelivery);
+            double Price = item.Price;
+            DateTime date = item.DateofLaunch;
+            int active = Convert.ToInt32(item.Active);
+            string cmd = "UPDATE menuitem SET Name='"+Name+"',FreeDelivery="+fd+",Price="+Price+",DateOfLaunch='"+date+"',Active="+active+" WHERE Id="+id;
+            SqlCommand update = new SqlCommand(cmd,con);
+            update.ExecuteNonQuery();
+            con.Close();
+        }
+    }
+}
