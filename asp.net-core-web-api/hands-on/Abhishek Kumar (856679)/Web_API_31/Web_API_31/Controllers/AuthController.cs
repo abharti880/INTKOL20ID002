009628602afa869a9ed85649693b383b711b9d98
GemAbhishek <66356468+GemAbhishek@users.noreply.github.com>
2020-06-19 12:46:01 +0530

    Add files via upload

---
+++ asp.net-core-web-api/hands-on/Abhishek Kumar (856679)/Web_API_31/Web_API_31/Controllers/AuthController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Web_API_31.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [AllowAnonymous]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public IActionResult Get(int id, [FromBody]string Role)
+        {
+            var Token = GenerateJSONWebToken(id, Role);
+            return Ok(Token);
+        }
+
+        private string GenerateJSONWebToken(int userId, string userRole)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("mysuperdupersecret"));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Role, userRole),
+                new Claim("UserId", userId.ToString())
+            };
+            var token = new JwtSecurityToken(
+            issuer: "mySystem",
+            audience: "myUsers",
+            claims: claims,
+            expires: DateTime.Now.AddMinutes(2),
+            signingCredentials: credentials);
+            var time = DateTime.Now;
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
